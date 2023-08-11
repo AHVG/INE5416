e6f959fd3bda771bec347658be821e792a6f12ff
Augusto de H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-08-11 11:36:56 -0300

    Adicionando todo

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ test_cases = input_data.strip().split('\n\n')
   
 program_to_run =  question_code + '.py' 
 for i, test_case in enumerate(test_cases, 1): 
+    # TODO: Criar um arquivo de saída também para facilitar a depuração
     output = sub.check_output(['python3', f'{program_to_run}'], input=test_case, universal_newlines=True) 
 
     print(f'### TEST {i}:\n') 
