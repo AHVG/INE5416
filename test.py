0985d73e489944cb6dd9ed37f4d7bbf3abaad56b
Augusto de H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-08-11 11:36:04 -0300

    Modificação da estrutura de pastas e do teste automatizado

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import subprocess as sub 
 import sys 
   
-question_code = sys.argv[1] 
-input_path = 'inputs/' + question_code + '.in' 
+question_code = sys.argv[1]
+input_path = 'inputs/' + question_code.split("/")[-1] + '.in' 
 with open(input_path, 'r') as input_file: 
      input_data = input_file.read()
   
 test_cases = input_data.strip().split('\n\n') 
   
-program_to_run = './exercises/' + question_code + '.py' 
+program_to_run =  question_code + '.py' 
 for i, test_case in enumerate(test_cases, 1): 
     output = sub.check_output(['python3', f'{program_to_run}'], input=test_case, universal_newlines=True) 
 
