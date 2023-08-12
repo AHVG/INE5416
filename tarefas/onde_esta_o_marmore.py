dd2075f3849b7edcb5abb1796cef34d50b08d029
Augusto de H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-08-12 13:36:18 -0300

    Marmore arrumado

--- tarefas/onde_esta_o_marmore.py
+++ tarefas/onde_esta_o_marmore.py
@@ -1,3 +1,4 @@
+import bisect
 caso = 1
 while True:
     pecas = []
@@ -6,14 +7,12 @@ while True:
         break
     for _ in range(N):
         pecas.append(int(input()))
+    pecas.sort()
     print(f"CASE# {caso}:")
     for _ in range(Q):
         peca = int(input())
-        indice = 0
-        if peca in pecas:
-            for p in pecas:
-                if p < peca:
-                    indice += 1
+        indice = bisect.bisect_left(pecas, peca)
+        if indice < len(pecas) and pecas[indice] == peca:
             print(f"{peca} found at {indice + 1}")
         else:
             print(f"{peca} not found")
--- tarefas/trilhos.py
+++ tarefas/trilhos.py
@@ -2,18 +2,18 @@ while True:
     numero_vagoes = int(input())
     if numero_vagoes == 0:
         break
-    vagoes = [i for i in range(1, numero_vagoes + 1)]
+    vagoes = [i for i in range(numero_vagoes, 0, -1)]
     while True:
         vagoes_depois = input().split()
         if len(vagoes_depois) == 1:
             break
-        vagoes_depois = [int(x) for x in vagoes_depois]
+        vagoes_depois = [int(x) for x in vagoes_depois[::-1]]
 
         vagoes_antes = []
         while len(vagoes_depois):
             buffer = []
             buffer.append(vagoes_depois.pop(0))
-            while len(vagoes_depois) and buffer[-1] == 1 + vagoes_depois[0]:
+            while len(vagoes_depois) and buffer[-1] == vagoes_depois[0] - 1:
                 buffer.append(vagoes_depois.pop(0))
             vagoes_antes.extend(buffer[::-1])
         print("Yes") if all([a == b for a, b in zip(vagoes, vagoes_antes)]) else print("No")
