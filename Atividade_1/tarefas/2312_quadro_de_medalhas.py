707b3b13ce11656b15aacbd553000efceccb7698
Augusto de H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-08-19 18:42:15 -0300

    Melhorando o 2312

--- Atividade_1/tarefas/2312_quadro_de_medalhas.py
+++ Atividade_1/tarefas/2312_quadro_de_medalhas.py
@@ -1,16 +1,1 @@
-from dataclasses import dataclass
-
-@dataclass
-class Pais:
-  nome: str
-  ouro: int
-  prata: int
-  bronze: int
-
-def criar_pais(nome, ouro, prata, bronze):
-    return Pais(nome, int(ouro), int(prata), int(bronze))
-
-def conversor_de_nome(nome):
-   return "".join([chr(ord('z') - (ord(letra) - ord('a'))) for letra in nome.lower()])
-   
-[print(f"{pais.nome} {pais.ouro} {pais.prata} {pais.bronze}") for pais in sorted([criar_pais(*input().split()) for i in range(int(input()))], key=lambda pais: (pais.ouro, pais.prata, pais.bronze, conversor_de_nome(pais.nome)), reverse=True)]
+[print(f"{pais['n']} {pais['o']} {pais['p']} {pais['b']}") for pais in sorted([(lambda n, o, p, b: {"n": n, "o": int(o), "p": int(p), "b": int(b)})(*input().split()) for i in range(int(input()))], key=lambda pais: (pais["o"], pais["p"], pais["b"], (lambda n: "".join([chr(ord('z') - (ord(letra) - ord('a'))) for letra in n.lower()]))(pais["n"])), reverse=True)]
