d5c0749afeeb830eca24db395c396ca3a9a645d6
Augusto de H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-08-19 17:04:17 -0300

    Ainda não melhorou

--- tarefas/menor_da_pilha.py
+++ tarefas/menor_da_pilha.py
@@ -1,13 +1,19 @@
 
 presentes = []
+min_presente = [10**9]
 for _ in range(int(input())):
     operacao = input().split()
     if operacao[0] == "PUSH":
-        presentes.append(int(operacao[1]))
+        valor = int(operacao[1])
+        presentes.append(valor)
+        if min_presente[-1] >= valor:
+            min_presente.append(valor)
     elif len(presentes):
         if operacao[0] == "POP":
+            if presentes[-1] == min_presente[-1]:
+                min_presente.pop()
             presentes.pop()
         else:
-            print(min(presentes))
+            print(min_presente[-1])
     else:
         print("EMPTY")
