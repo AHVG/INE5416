1a1e99f6627c01d89775f2c99db416f8dbe73d21
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-10-01 10:24:24 -0300

    Ainda não testado]

--- Trabalho_1/main.py
+++ Trabalho_1/main.py
@@ -11,34 +11,44 @@ def fazer_tabuleiro_sinais(resposta):
 
     for i, linha in enumerate(resposta):
         for j, elemento in enumerate(linha):
-            sinais_elemento = {"E": NADA, "D": NADA, "B": NADA, "C": NADA}
+            sinais_elemento = {"E": NADA, "D": NADA, "B": NADA, "C": NADA, "MAIORES": 0, "MENORES": 0}
             x, y = j, i - 1
             if -1 < x < 9 and -1 < y < 9:
                 if elemento > resposta[y][x]:
                     sinais_elemento["C"] = MAIOR
+                    sinais_elemento["MAIORES"] += 1
                 else:
                     sinais_elemento["C"] = MENOR
+                    sinais_elemento["MENORES"] += 1
 
             x, y = j + 1, i
             if -1 < x < 9 and -1 < y < 9:
                 if elemento > resposta[y][x]:
                     sinais_elemento["D"] = MAIOR
+                    sinais_elemento["MAIORES"] += 1
                 else:
                     sinais_elemento["D"] = MENOR
+                    sinais_elemento["MENORES"] += 1
 
             x, y = j, i + 1
             if -1 < x < 9 and -1 < y < 9:
                 if elemento > resposta[y][x]:
                     sinais_elemento["B"] = MAIOR
+                    sinais_elemento["MAIORES"] += 1
                 else:
                     sinais_elemento["B"] = MENOR
+                    sinais_elemento["MENORES"] += 1
             
             x, y = j - 1, i
             if -1 < x < 9 and -1 < y < 9:
                 if elemento > resposta[y][x]:
                     sinais_elemento["E"] = MAIOR
+                    sinais_elemento["MAIORES"] += 1
                 else:
                     sinais_elemento["E"] = MENOR
+                    sinais_elemento["MENORES"] += 1
+    
+            resultado[i][j] = sinais_elemento.copy()
 
     return resultado
 
@@ -52,10 +62,40 @@ def eh_solucao(estado):
 
 
 def obter_possibilidades(estado, posicao):
-    possibilidades = [i for i in range(1, 10)]
     x, y = posicao
+
     # Maior Menor
+    sinais_elemento = sinais[y][x]
+    maior = 9 - sinais_elemento["MAIORES"]
+    menor = sinais_elemento["MENORES"]
+    if sinais_elemento["C"] == MAIOR:
+        if maior > estado[y - 1][x]:
+            maior = estado[y - 1][x]
+    elif sinais_elemento["C"] == MENOR:
+        if menor < estado[y - 1][x]:
+            menor = estado[y - 1][x]
     
+    if sinais_elemento["D"] == MAIOR:
+        if maior > estado[y][x + 1]:
+            maior = estado[y][x + 1]
+    elif sinais_elemento["D"] == MENOR:
+        if menor < estado[y][x + 1]:
+            menor = estado[y][x + 1]
+
+    if sinais_elemento["B"] == MAIOR:
+        if maior > estado[y + 1][x]:
+            maior = estado[y + 1][x]
+    elif sinais_elemento["B"] == MENOR:
+        if menor < estado[y + 1][x]:
+            menor = estado[y + 1][x]
+
+    if sinais_elemento["E"] == MAIOR:
+        if maior > estado[y][x - 1]:
+            maior = estado[y][x - 1]
+    elif sinais_elemento["E"] == MENOR:
+        if menor < estado[y][x - 1]:
+            menor = estado[y][x - 1]
+    possibilidades = [i for i in range(menor + 1, maior)]
 
     # linha e coluna
     for k in range(9):
@@ -63,6 +103,7 @@ def obter_possibilidades(estado, posicao):
             possibilidades.remove(estado[k][x])
         if k != x and estado[y][k] in possibilidades:
             possibilidades.remove(estado[y][k])
+
     # bloco
     for i in range(3):
         for j in range(3):
@@ -70,6 +111,7 @@ def obter_possibilidades(estado, posicao):
             coluna = x // 3
             if linha != y and coluna != x and estado[linha + i][coluna + j] in possibilidades:
                 possibilidades.remove(estado[linha + i][coluna + j])
+
     return copy.copy(possibilidades)
 
 
