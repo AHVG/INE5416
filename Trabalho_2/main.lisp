b75e136bedc3cd757eb43ad1c2cac5fea534a184
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-10-14 14:13:47 -0300

    Está falhando alguma verificação

--- Trabalho_2/main.lisp
+++ Trabalho_2/main.lisp
@@ -1,49 +1,161 @@
 
-(defvar solucao (make-array 81 :initial-element 0))
 (defvar comparacao (list (list -1 1 1 -1) (list -1 0 0 0) (list -1 -1 0 1) (list -1 1 1 -1) (list -1 0 0 0) (list -1 -1 0 1) (list -1 0 0 -1) (list -1 0 1 1) (list -1 -1 1 1) (list 0 1 1 -1) (list 1 0 1 0) (list 1 -1 0 1) (list 0 0 0 -1) (list 1 1 1 1) (list 1 -1 1 0) (list 1 1 0 -1) (list 0 1 0 0) (list 0 -1 0 0) (list 0 1 -1 -1) (list 0 0 -1 0) (list 1 -1 -1 1) (list 1 1 -1 -1) (list 0 1 -1 0) (list 0 -1 -1 0) (list 1 0 -1 -1) (list 1 1 -1 1) (list 1 -1 -1 0) (list -1 0 1 -1) (list -1 1 0 1) (list -1 -1 0 0) (list -1 1 1 -1) (list -1 1 0 0) (list -1 -1 0 0) (list -1 0 0 -1) (list -1 1 1 1) (list -1 -1 0 0) (list 0 0 0 -1) (list 1 1 1 1) (list 1 -1 1 0) (list 0 0 0 -1) (list 1 1 1 1) (list 1 -1 0 0) (list 1 1 0 -1) (list 0 0 0 0) (list 1 -1 1 1) (list 1 0 -1 -1) (list 0 1 -1 1) (list 0 -1 -1 0) (list 1 0 -1 -1) (list 0 1 -1 1) (list 1 -1 -1 0) (list 1 1 -1 -1) (list 1 1 -1 0) (list 0 -1 -1 0) (list -1 1 0 -1) (list -1 1 0 0) (list -1 -1 0 0) (list -1 0 0 -1) (list -1 0 1 1) (list -1 -1 1 1) (list -1 1 1 -1) (list -1 0 0 0) (list -1 -1 0 1) (list 1 1 1 -1) (list 1 0 1 0) (list 1 -1 1 1) (list 1 1 0 -1) (list 0 0 0 0) (list 0 -1 0 1) (list 0 0 0 -1) (list 1 0 0 1) (list 1 -1 0 1) (list 0 0 -1 -1) (list 0 1 -1 1) (list 0 -1 -1 0) (list 1 1 -1 -1) (list 1 1 -1 0) (list 1 -1 -1 0) (list 1 0 -1 -1) (list 1 0 -1 1) (list 1 -1 -1 1)))
 (defvar visitados (list ))
 
+
 (defun limite (posicao comp)
-  (let ((total 1))
-    (push posicao visitados)
-    
-    (if (and (= (elt (elt comparacao posicao) 0) comp) (not (member (- posicao 9) visitados)))
-        (setq total (+ total (limite (- posicao 9) comp))))
-    
-    (if (and (= (elt (elt comparacao posicao) 1) comp) (not (member (+ posicao 1) visitados)))
-        (setq total (+ total (limite (+ posicao 1) comp))))
-    
-    (if (and (= (elt (elt comparacao posicao) 2) comp) (not (member (+ posicao 9) visitados)))
-        (setq total (+ total (limite (+ posicao 9) comp))))
-    
-    (if (and (= (elt (elt comparacao posicao) 3) comp) (not (member (- posicao 1) visitados)))
-        (setq total (+ total (limite (- posicao 1) comp))))
-    
-    total)
-)
-
-
-(defun matriz-possibilidades ()
-  (let ((matriz '()))
-    (loop for i from 0 to 80 do
-      (let ((linha '()) (limite-inferior 1) (limite-superior 9))
-        (setq visitados (list ))
-        (setq limite-inferior (limite i 1))
-
-        (setq visitados (list ))
-        (setq limite-superior (limite i 0))
+    (let ((total 1))
+        (push posicao visitados)
         
-        (loop for j from limite-inferior to (- 10 limite-superior) do
-          (push j linha))
-        (push (reverse linha) matriz)))
-    (reverse matriz))
+        (if (and (= (elt (elt comparacao posicao) 0) comp) (not (member (- posicao 9) visitados)))
+            (setq total (+ total (limite (- posicao 9) comp))))
+        
+        (if (and (= (elt (elt comparacao posicao) 1) comp) (not (member (+ posicao 1) visitados)))
+            (setq total (+ total (limite (+ posicao 1) comp))))
+        
+        (if (and (= (elt (elt comparacao posicao) 2) comp) (not (member (+ posicao 9) visitados)))
+            (setq total (+ total (limite (+ posicao 9) comp))))
+        
+        (if (and (= (elt (elt comparacao posicao) 3) comp) (not (member (- posicao 1) visitados)))
+            (setq total (+ total (limite (- posicao 1) comp))))
+        
+        total)
 )
+
+
+(defun calcular-possibilidades ()
+    (let ((solucao '()))
+        (loop for i from 0 to 80 do
+        (let ((linha '()) (limite-inferior 1) (limite-superior 9))
+            ; Visitados está comom global, pois os parâmetros de uma função é uma copia e não referência 
+            ; Seta ele como vazio para calcular o limite inferior
+            (setq visitados (list ))
+            (setq limite-inferior (limite i 1))
+
+            ; Seta ele como vazio para calcular o limite superior
+            (setq visitados (list ))
+            (setq limite-superior (limite i 0))
+            
+            (loop for j from limite-inferior to (- 10 limite-superior) do
+            (push j linha))
+            (push (reverse linha) solucao)))
+        (reverse solucao))
+)
+
+
+(defvar solucao (make-array 81 :initial-element 0))
+(defvar possibilidades ( calcular-possibilidades ))
+
+
+(defun repete-linha (elemento posicao)
+    (loop for i from 0 to 8 do
+        (if (= (elt solucao (+ i (* 9 (floor posicao 9)))) elemento) 
+            (return t)))
+    nil
+)
+
+
+(defun repete-coluna (elemento posicao)
+    (loop for i from 0 to 8 do
+        (if (= (elt solucao (+ (* 9 i) (mod posicao 9))) elemento) 
+            (return t)))
+    nil
+)
+
+
+(defun repete-regiao (elemento posicao)
+    ;; (print posicao)
+    (let ((linha (* 3 (floor (/ posicao 27))))
+        (coluna (* 3 (floor (/ (mod posicao 9) 3)))))
+    (loop for i from linha below (+ linha 3) do
+        (loop for j from coluna below (+ coluna 3) do
+            (if (= (elt solucao (+ (* 9 i) j)) elemento)
+                (return t))))
+    nil)
+)
+
+
+(defun compara (elemento posicao)
+    (cond
+        ((= (elt (elt comparacao posicao) 0) 1)
+        (if (and (< elemento (elt solucao (- posicao 9)))
+                (elt solucao (- posicao 9)))
+            (return-from compara nil)))
+
+        ((= (elt (elt comparacao posicao) 0) 0)
+        (if (and (> elemento (elt solucao (- posicao 9)))
+                (elt solucao (- posicao 9)))
+            (return-from compara nil)))
+    )
+    (cond
+        ((= (elt (elt comparacao posicao) 1) 1)
+        (if (and (< elemento (elt solucao (+ posicao 1)))
+                (elt solucao (+ posicao 1)))
+            (return-from compara nil)))
+
+        ((= (elt (elt comparacao posicao) 1) 0)
+        (if (and (> elemento (elt solucao (+ posicao 1)))
+                (elt solucao (+ posicao 1)))
+            (return-from compara nil)))
+    )
+    (cond
+        ((= (elt (elt comparacao posicao) 2) 1)
+        (if (and (< elemento (elt solucao (+ posicao 9)))
+                (elt solucao (+ posicao 9)))
+            (return-from compara nil)))
+
+        ((= (elt (elt comparacao posicao) 2) 0)
+        (if (and (> elemento (elt solucao (+ posicao 9)))
+                (elt solucao (+ posicao 9)))
+            (return-from compara nil)))
+    )
+    (cond
+        ((= (elt (elt comparacao posicao) 3) 1)
+        (if (and (< elemento (elt solucao (- posicao 1)))
+                (elt solucao (- posicao 1)))
+            (return-from compara nil)))
+
+        ((= (elt (elt comparacao posicao) 3) 0)
+        (if (and (> elemento (elt solucao (- posicao 1)))
+                (elt solucao (- posicao 1)))
+            (return-from compara nil)))
+
+    )
+    t
+)
+
+
+(defun resolve (elemento posicao)
+    (cond
+        ((repete-linha elemento posicao) nil)
+        ((repete-coluna elemento posicao) nil)
+        ((repete-regiao elemento posicao) nil)
+        ((not (compara elemento posicao)) nil)
+        ((= posicao 80)
+            (setf (elt solucao posicao) elemento)
+            t
+        )
+        (t
+            (setf (elt solucao posicao) elemento)
+            (print solucao)
+            (loop for possibilidade in (elt possibilidades (+ posicao 1)) do
+                (if (resolve possibilidade (+ posicao 1)) (return t)))
+            (setf (elt solucao posicao) 0)
+            nil
+        )
+    )
+)
+
 
 
 (defun main()
-    (write-line (write-to-string solucao))
     (write-line (write-to-string comparacao))
-    (write-line (write-to-string ( matriz-possibilidades )))
+    (write-line (write-to-string solucao))
+    (write-line (write-to-string possibilidades))
+    (loop for possibilidade in (elt possibilidades 0) do
+        (if (resolve possibilidade 0)
+            (write-line (write-to-string solucao))
+            t))
 )
 
 (main)
