d74aa34f48858994b6ee6884982884b9d7b17fd3
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-10-14 12:20:34 -0300

    Feito as matrizes

--- Trabalho_2/main.lisp
+++ Trabalho_2/main.lisp
@@ -1,42 +1,49 @@
 
 (defvar solucao (make-array 81 :initial-element 0))
 (defvar comparacao (list (list -1 1 1 -1) (list -1 0 0 0) (list -1 -1 0 1) (list -1 1 1 -1) (list -1 0 0 0) (list -1 -1 0 1) (list -1 0 0 -1) (list -1 0 1 1) (list -1 -1 1 1) (list 0 1 1 -1) (list 1 0 1 0) (list 1 -1 0 1) (list 0 0 0 -1) (list 1 1 1 1) (list 1 -1 1 0) (list 1 1 0 -1) (list 0 1 0 0) (list 0 -1 0 0) (list 0 1 -1 -1) (list 0 0 -1 0) (list 1 -1 -1 1) (list 1 1 -1 -1) (list 0 1 -1 0) (list 0 -1 -1 0) (list 1 0 -1 -1) (list 1 1 -1 1) (list 1 -1 -1 0) (list -1 0 1 -1) (list -1 1 0 1) (list -1 -1 0 0) (list -1 1 1 -1) (list -1 1 0 0) (list -1 -1 0 0) (list -1 0 0 -1) (list -1 1 1 1) (list -1 -1 0 0) (list 0 0 0 -1) (list 1 1 1 1) (list 1 -1 1 0) (list 0 0 0 -1) (list 1 1 1 1) (list 1 -1 0 0) (list 1 1 0 -1) (list 0 0 0 0) (list 1 -1 1 1) (list 1 0 -1 -1) (list 0 1 -1 1) (list 0 -1 -1 0) (list 1 0 -1 -1) (list 0 1 -1 1) (list 1 -1 -1 0) (list 1 1 -1 -1) (list 1 1 -1 0) (list 0 -1 -1 0) (list -1 1 0 -1) (list -1 1 0 0) (list -1 -1 0 0) (list -1 0 0 -1) (list -1 0 1 1) (list -1 -1 1 1) (list -1 1 1 -1) (list -1 0 0 0) (list -1 -1 0 1) (list 1 1 1 -1) (list 1 0 1 0) (list 1 -1 1 1) (list 1 1 0 -1) (list 0 0 0 0) (list 0 -1 0 1) (list 0 0 0 -1) (list 1 0 0 1) (list 1 -1 0 1) (list 0 0 -1 -1) (list 0 1 -1 1) (list 0 -1 -1 0) (list 1 1 -1 -1) (list 1 1 -1 0) (list 1 -1 -1 0) (list 1 0 -1 -1) (list 1 0 -1 1) (list 1 -1 -1 1)))
+(defvar visitados (list ))
 
-
-(defun limite (posicao visitados comp)
+(defun limite (posicao comp)
   (let ((total 1))
     (push posicao visitados)
     
     (if (and (= (elt (elt comparacao posicao) 0) comp) (not (member (- posicao 9) visitados)))
-        (setq total (+ total (limite (- posicao 9) visitados comp))))
+        (setq total (+ total (limite (- posicao 9) comp))))
     
     (if (and (= (elt (elt comparacao posicao) 1) comp) (not (member (+ posicao 1) visitados)))
-        (setq total (+ total (limite (+ posicao 1) visitados comp))))
+        (setq total (+ total (limite (+ posicao 1) comp))))
     
     (if (and (= (elt (elt comparacao posicao) 2) comp) (not (member (+ posicao 9) visitados)))
-        (setq total (+ total (limite (+ posicao 9) visitados comp))))
+        (setq total (+ total (limite (+ posicao 9) comp))))
     
     (if (and (= (elt (elt comparacao posicao) 3) comp) (not (member (- posicao 1) visitados)))
-        (setq total (+ total (limite (- posicao 1) visitados comp))))
+        (setq total (+ total (limite (- posicao 1) comp))))
     
-    total))
+    total)
+)
 
 
 (defun matriz-possibilidades ()
   (let ((matriz '()))
     (loop for i from 0 to 80 do
-      (let ((linha '()))
-        (loop for j from (+ 1 (limite i '() 1)) to (- 10 (limite i '() 0)) do
+      (let ((linha '()) (limite-inferior 1) (limite-superior 9))
+        (setq visitados (list ))
+        (setq limite-inferior (limite i 1))
+
+        (setq visitados (list ))
+        (setq limite-superior (limite i 0))
+        
+        (loop for j from limite-inferior to (- 10 limite-superior) do
           (push j linha))
         (push (reverse linha) matriz)))
-    matriz))
+    (reverse matriz))
+)
 
 
 (defun main()
     (write-line (write-to-string solucao))
     (write-line (write-to-string comparacao))
     (write-line (write-to-string ( matriz-possibilidades )))
-    
 )
 
 (main)
