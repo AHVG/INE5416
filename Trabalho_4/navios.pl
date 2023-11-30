7998c5f8c67fe0a284cefcc0fd433f5b6b31adf5
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-11-29 21:09:44 -0300

    Top

--- Trabalho_4/navios.pl
+++ Trabalho_4/navios.pl
@@ -54,9 +54,6 @@ todosDiferentes([]).
 todosDiferentes([H|T]) :- not(member(H,T)), todosDiferentes(T).
 
 solucao(ListaSolucao) :- 
-
-    %Ver oq quer dizer exatamente a esquerda ou a direita
-
     ListaSolucao = [
         homem(Camiseta1, Dono1, Profissao1, Companhia1, Animal1, Idade1),
         homem(Camiseta2, Dono2, Profissao2, Companhia2, Animal2, Idade2),
@@ -66,7 +63,7 @@ solucao(ListaSolucao) :-
     ],
 
     %O animal de 2 anos está na primeira posição
-    Idade2 = 2,
+    Idade1 = 2,
 
     %O dono que está acompanhado da Irmã está ao lado do dono da camiseta Vermelha
     aoLado(homem(_, _, _, irma, _, _), homem(vermelha, _, _, _, _, _), ListaSolucao),
@@ -81,22 +78,23 @@ solucao(ListaSolucao) :-
     aoLado(homem(victor, _, _, _, _, _), homem(_, _, _, mae, _, _), ListaSolucao),
 
     %Sidney está em uma das pontas
-    noCanto(navio(sidney, _, _, _, _, _), ListaSolucao),
+    noCanto(homem(sidney, _, _, _, _, _), ListaSolucao),
 
     %O Mini porco está exatamente à direita do animal mais velho
     aDireita(homem(_, _, _, _, mini_porco, _), homem(_, _, _, _, _, 5), ListaSolucao),
+    aoLado(homem(_, _, _, _, mini_porco, _), homem(_, _, _, _, _, 5), ListaSolucao),
 
     %O dono de camiseta Azul é Joalheiro
-    member(homem(azul, _, joalheiro, _, _, _), ListaSolucao)
+    member(homem(azul, _, joalheiro, _, _, _), ListaSolucao),
 
     %O Sociólogo está ao lado do dono que tem o animal mais novo
     aoLado(homem(_, _, sociologo, _, _, _), homem(_, _, _, _, 1, _), ListaSolucao),
 
     %Raphael está na quinta posição
-    Dono5 = raphael
+    Dono5 = raphael,
 
     %O Cachorro tem 4 anos
-    member(homem(_, _, _, _, cachorro, 4), ListaSolucao)
+    member(homem(_, _, _, _, cachorro, 4), ListaSolucao),
 
     %Em uma das pontas está o homem que está acompanhado do Filho
     noCanto(navio(_, _, _, filho, _, _), ListaSolucao),
@@ -105,7 +103,7 @@ solucao(ListaSolucao) :-
     aoLado(homem(_, _, sociologo, _, _, _), homem(_, _, _, filho, _, _), ListaSolucao),
 
     %Caio tem um Gato
-    member(homem(_, caio, _, _, gato, _), ListaSolucao)
+    member(homem(_, caio, _, _, gato, _), ListaSolucao),
 
     %O rapaz de Azul está em algum lugar à esquerda do dono do animal de 5 anos
     aEsquerda(homem(azul, _, _, _, _, _), homem(_, _, _, _, _, 5), ListaSolucao),
@@ -114,25 +112,26 @@ solucao(ListaSolucao) :-
     aoLado(homem(amarela, _, _, _, _, _), homem(_, _, _, mae, _, _), ListaSolucao),
 
     %O animal de 4 anos está na terceira posição
-    Idade3 = 4
+    Idade3 = 4,
 
     %A Iguana tem 3 anos
-    member(homem(_, _, _, _, iguana, 3), ListaSolucao)
+    member(homem(_, _, _, _, iguana, 3), ListaSolucao),
 
     %O homem de Branco está exatamente à esquerda de Victor
-    aEsquerda(homem(branco, _, _, _, _, _), homem(_, victor, _, _, _, 5_), ListaSolucao),
+    aEsquerda(homem(branco, _, _, _, _, _), homem(_, victor, _, _, _, 5), ListaSolucao),
+    aoLado(homem(branco, _, _, _, _, _), homem(_, victor, _, _, _, 5), ListaSolucao),
 
     %Na terceira posição está o dono que está acompanhado da Irmã
-    Companhia3 = irma
+    Companhia3 = irma,
 
     %O Designer tem uma Iguana
-    member(homem(_, _, designer, _, iguana, _), ListaSolucao)
+    member(homem(_, _, designer, _, iguana, _), ListaSolucao),
 
     %O Joalheiro e o Sociólogo estão lado a lado
-    aoLado(homem(_, _, joalheiro, _, _, _), homem(_, _, sociologo, , _, _), ListaSolucao),
+    aoLado(homem(_, _, joalheiro, _, _, _), homem(_, _, sociologo, _, _, _), ListaSolucao),
 
     %O rapaz que está acompanhado da Mãe é dono do animal de 5 anos
-    member(homem(_, _, _, mae, _, 5), ListaSolucao)
+    member(homem(_, _, _, mae, _, 5), ListaSolucao),
     
     %Testa todas as possibilidades...
     camiseta(Camiseta1), camiseta(Camiseta2), camiseta(Camiseta3), camiseta(Camiseta4), camiseta(Camiseta5),
@@ -148,8 +147,10 @@ solucao(ListaSolucao) :-
     todosDiferentes([Companhia1, Companhia2, Companhia3, Companhia4, Companhia5]),
     
     animal(Animal1), animal(Animal2), animal(Animal3), animal(Animal4), animal(Animal5),
-    todosDiferentes([Animal1, Animal2, Animal3, Animal4, Animal5]).
+    todosDiferentes([Animal1, Animal2, Animal3, Animal4, Animal5]),
 
     idade(Idade1), idade(Idade2), idade(Idade3), idade(Idade4), idade(Idade5),
     todosDiferentes([Idade1, Idade2, Idade3, Idade4, Idade5]).
-    
+
+
+?- solucao(L), write(L).
